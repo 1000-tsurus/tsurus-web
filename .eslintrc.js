a59e35edc9aa41067f423548295434d60f72b029
Guilherme Iuri <71740621+guiliuri13@users.noreply.github.com>
2022-02-18 23:29:03 -0300

    fixxes for package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
-    extends: ['react-app'],
+    plugins: ['prettier'],
     rules: {
         '@typescript-eslint/no-explicit-any': 2,
         "@typescript-eslint/naming-convention": "off",
-	  	  "brace-style": ["error", "allman"],
+        "brace-style": ["error", "allman"],
         "indent": ["error", 4],
         "@typescript-eslint/indent": ["error", 4],
         "@typescript-eslint/semi": "off",
         "semi": ["error", "always"]
-    }
-};
+}
