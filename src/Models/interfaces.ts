0b828cab7a2d689d56bcead9f974191f7a609ae4
guilherme <guiliuri13@gmail.com>
2022-06-19 17:17:20 -0300

    init of users

--- src/Models/interfaces.ts
+++ src/Models/interfaces.ts
@@ -1,1 +1,46 @@
-export type User = {}
+export type User = {
+    id: number
+    full_name: string
+    email: string
+    icon_url: string
+    type_id: number
+    created_at: string
+    updated_at: string
+    abouts: any[]
+    employer: {
+        id: number
+        role: string
+        employer: string;
+        entry_date_time: string;
+        created_at: string;
+        updated_at: string;
+    }[]
+    contact: any[]
+    occupation: any[]
+    skill_category: {
+        id: number;
+        skill_name: string;
+        created_at: string;
+        updated_at: string;
+    }[]
+    to_help: {
+        id: number
+        to_help_text: string
+        created_at: string
+    }[]
+    trajectory: {
+        id: number
+        trajectory_text: string
+        created_at: string
+    }[]
+    phone: {
+        id: number
+        phone_number: string
+        ddd: string
+        country_code: string 
+        is_wpp: number
+        is_public: number
+        created_at: string
+        updated_at: string
+    }[]
+}
--- src/Pages/User/User.tsx
+++ src/Pages/User/User.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { User } from '@/Models/interfaces'
+import { api } from '@/Services/api'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
-export default function User() {
-  return (
-    <div>User page</div>
-  )
+export default function User() {	
+	const [userData, setUserData] = useState<User>(),
+		[isLoading, setIsLoading] = useState<boolean>(true),
+		{userId} = useParams()
+
+	useEffect(() => {
+		setIsLoading(true)
+		api.get(`user/${userId}`)
+			.then((res) => {
+				setUserData(res.data)
+			})
+			.catch((err) => {
+				console.log(err);
+				toast.error(
+					'Erro ao caregar informações do usuário. Por favor, tente novamente mais tarde',
+					{theme: 'colored'}
+				)
+			})
+			.finally(() => setIsLoading(false))
+	}, [])
+
+	return (
+		<div>User page</div>
+	)
 }
